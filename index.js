40f8ed47598bdde67f58f285b5bd1cba3c31e819
danisanz <daniDNZ@users.noreply.github.com>
2022-08-17 16:43:30 +0200

    All tests passed

--- index.js
+++ index.js
@@ -6,23 +6,42 @@ class Room {
     this.discount = discount;
   }
 
-  isOccupied(date) {
-    // const targetDate = new Date(date)
-    // let name
-    // this.bookings.forEach((element) => {
-    //   const checkin = new Date(element.checkin)
-    //   const checkout = new Date(element.checkout)
-    //   if (checkin <= targetDate && checkout >= targetDate) {
-    //     name = element.name
-    //   }
-    // });
+  dateRange(startDate, endDate) {
+    let start = new Date(startDate)
+    const end = new Date(endDate)
+    let dateRange = []
 
-    // return name ? name : false
-    return
+    while (start <= end) {
+      dateRange = [...dateRange, start.toISOString()]
+      start.setDate(start.getDate() + 1)
+    }
+    return dateRange
+  }
+
+  isOccupied(date) {
+    const targetDate = new Date(date)
+    let name
+    this.bookings.forEach((element) => {
+      const checkin = new Date(element.checkin)
+      const checkout = new Date(element.checkout)
+      if (checkin <= targetDate && checkout >= targetDate) {
+        name = element.name
+      }
+    });
+
+    return name ? name : false
   }
 
   occupancyPercentage(startDate, endDate) {
+    const dateRange = this.dateRange(startDate, endDate)
+    let daysOccuped = 0;
 
+    dateRange.forEach(date => {
+      if (this.isOccupied(date) !== false) daysOccuped += 1
+    })
+
+    const percentage = ((daysOccuped * 100) / dateRange.length)
+    return percentage
   }
 }
 
@@ -36,18 +55,37 @@ class Booking {
     this.room = room;
   }
 
-  fee() {
+  getFee() {
+    const totalDiscount = this.discount + this.room.discount
+    const dateRange = this.room.dateRange(this.checkin, this.checkout)
+    const totalPrice = dateRange.length * this.room.rate
+    const priceWithDiscount = (totalPrice - ((totalDiscount / 100) * totalPrice));
 
+    console.log(totalDiscount, dateRange.length, totalPrice, priceWithDiscount)
+
+    return priceWithDiscount
   }
 }
 
 function totalOccupancyPercentage(rooms, startDate, endDate) {
+  let totalOccupancyPercentage = 0
+  rooms.forEach(room => {
+    totalOccupancyPercentage += room.occupancyPercentage(startDate, endDate) / rooms.length
+  })
 
+  return Math.round(totalOccupancyPercentage)
 }
 
 function availableRooms(rooms, startDate, endDate) {
-
+  const arrAvailableRooms = []
+  rooms.forEach(room => {
+    if (room.occupancyPercentage(startDate, endDate) === 0) {
+      arrAvailableRooms.push(room)
+    }
+  })
+  return arrAvailableRooms.length > 0 ? arrAvailableRooms : false
 }
+
 
 module.exports = {
   Room, Booking, totalOccupancyPercentage, availableRooms
--- index.test.js
+++ index.test.js
@@ -17,33 +17,74 @@ describe("Room", () => {
 
   test('occupancyPercentage', () => {
     const booking1 = new Booking({ ...bookingTemplate })
+    const booking2 = new Booking({ ...bookingTemplate, checkin: '2022-01-12', checkout: '2022-01-14' })
+    const room1 = new Room({ ...roomTemplate, bookings: [booking1, booking2] })
+    expect(room1.occupancyPercentage('2022-01-03', '2022-01-12')).toBe(90)
+  })
+
+  test('occupancyPercentage is 0', () => {
+    const booking1 = new Booking({ ...bookingTemplate })
     const room1 = new Room({ ...roomTemplate, bookings: [booking1] })
-    expect(room1.occupancyPercentage('2022-01-03', '2022-01-12')).toBe(80)
+    expect(room1.occupancyPercentage('2022-01-15', '2022-01-23')).toBe(0)
   })
 })
 
 describe('Booking', () => {
-  test('Métodos Booking', () => {
-    const booking1 = new Booking({ ...bookingTemplate })
-    const room1 = new Room({ ...roomTemplate, bookings: [booking1] })
-    expect(room1.getFee()).toBe(208)
+  test('Booking Fee', () => {
+    let room1 = new Room({ ...roomTemplate })
+    const booking1 = new Booking({ ...bookingTemplate, room: room1 })
+    room1.bookings.push(booking1)
+
+    expect(booking1.getFee()).toBe(208000)
+  })
+
+  test('Booking Fee 100% discount', () => {
+    let room1 = new Room({ ...roomTemplate })
+    const booking1 = new Booking({ ...bookingTemplate, room: room1, discount: 75 })
+    room1.bookings.push(booking1)
+
+    expect(booking1.getFee()).toBe(0)
   })
 })
 
-describe('Methods', () => {
-  test('Total Occupancy Percentage', () => {
-    const booking1 = new Booking({ ...bookingTemplate })
-    const room1 = new Room({ ...roomTemplate, bookings: [booking1] })
+describe('Total Occupancy Pecentage Method', () => {
+  const booking1 = new Booking({ ...bookingTemplate, checkout: '2022-01-14' })
+  const booking2 = new Booking({ ...bookingTemplate, checkin: '2022-01-12', checkout: '2022-01-14' })
+  const booking3 = new Booking({ ...bookingTemplate, checkin: '2022-01-08', checkout: '2022-01-14' })
+  const room1 = new Room({ ...roomTemplate, bookings: [booking1] })
+  const room2 = new Room({ ...roomTemplate, bookings: [booking2] })
+  const room3 = new Room({ ...roomTemplate, bookings: [booking3] })
+
+  test('Total Occupancy Percentage 33%', () => {
     const startDate = '2022-01-03'
-    const endDate = '2022-01-12'
-    expect(totalOccupancyPercentage([room1], startDate, endDate)).toBe(100)
+    const endDate = '2022-01-07'
+    expect(totalOccupancyPercentage([room1, room2, room3], startDate, endDate)).toBe(33)
   })
 
-  test('Available Rooms', () => {
-    const booking1 = new Booking({ ...bookingTemplate })
-    const room1 = new Room({ ...roomTemplate, bookings: [booking1] })
-    const startDate = '2022-01-03'
-    const endDate = '2022-01-12'
-    expect(availableRooms([room1], startDate, endDate)).toBe(0)
+  test('Total Occupancy Percentage 100%', () => {
+    const startDate = '2022-01-12'
+    const endDate = '2022-01-14'
+    expect(totalOccupancyPercentage([room1, room2, room3], startDate, endDate)).toBe(100)
+  })
+})
+
+describe('Available Rooms', () => {
+  const booking1 = new Booking({ ...bookingTemplate, checkout: '2022-01-14' })
+  const booking2 = new Booking({ ...bookingTemplate, checkin: '2022-01-12', checkout: '2022-01-14' })
+  const booking3 = new Booking({ ...bookingTemplate, checkin: '2022-01-08', checkout: '2022-01-14' })
+  const room1 = new Room({ ...roomTemplate, bookings: [booking1] })
+  const room2 = new Room({ ...roomTemplate, bookings: [booking2] })
+  const room3 = new Room({ ...roomTemplate, bookings: [booking3] })
+
+  test('Available Rooms false', () => {
+    const startDate = '2022-01-12'
+    const endDate = '2022-01-14'
+    expect(availableRooms([room1, room2, room3], startDate, endDate)).toBe(false)
+  })
+
+  test('Available Rooms 2', () => {
+    const startDate = '2022-01-06'
+    const endDate = '2022-01-07'
+    expect(availableRooms([room1, room2, room3], startDate, endDate)).toStrictEqual([room2, room3])
   })
 })
